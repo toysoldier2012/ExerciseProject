a9e0ed2c3684906e9650218eec231f9dfa3cde1d
Xiaoyu LI <toysoldier2012@gmail.com>
2018-09-13 16:58:32 +0200

    Revert "Revert "Revert "Revert "Maximum Product Subarray problem""""
    This reverts commit 7314ca9378ea72796672e7b48ee614a4184ba04e.

---
+++ longestSubstring/LongestSubstring.java
@@ -0,0 +1,19 @@
+package longestSubstring;
+
+import java.util.HashMap;
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {        
+        int iteratorPointer = 0, basePointer = 0;
+        int max = 0;
+        HashMap<Character, Integer> charMap = new HashMap<>();
+        
+        while(iteratorPointer < s.length()){
+            if(charMap.containsKey(s.charAt(iteratorPointer)))
+                basePointer = Math.max(basePointer, charMap.get(s.charAt(iteratorPointer)) + 1);
+            max = Math.max(max, iteratorPointer - basePointer +1);
+            charMap.put(s.charAt(iteratorPointer), iteratorPointer++);
+        }
+        return max;
+    }
+}

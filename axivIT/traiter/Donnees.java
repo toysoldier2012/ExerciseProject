946f8288e4fa47ba2c645b2b0a463e6d3c8a650e
Xiaoyu LI <toysoldier2012@gmail.com>
2019-02-09 10:28:27 +0100

    Revert "1"
    This reverts commit 167d198702ec52d9487b1367be1af6040ec067e0.

--- axivIT/traiter/Donnees.java
+++ axivIT/traiter/Donnees.java
@@ -7,10 +7,7 @@ import axivIT.tondeuseException;
 import axivIT.commune.Commune;
 import axivIT.commune.Erreur;
 <<<<<<< HEAD
-<<<<<<< HEAD
 import axivIT.entites.tondeuse;
-=======
->>>>>>> parent of b6e1b03... Finish!
 =======
 >>>>>>> parent of b6e1b03... Finish!
 
@@ -51,14 +48,11 @@ public class Donnees {
 		return true;
 	}
 <<<<<<< HEAD
-<<<<<<< HEAD
 
 	public static void resultat(Tondeuse instance) {
 		System.out.println("[ Touleuse: Coordonnees est: ["+ instance.getCoordonnestondeuse().toString() +
 				"], l'orientation est: "+ instance.getOrientationtondeuse().toString() +"]");
 	}
-=======
->>>>>>> parent of b6e1b03... Finish!
 =======
 >>>>>>> parent of b6e1b03... Finish!
 }
